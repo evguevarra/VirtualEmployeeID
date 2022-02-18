7abbdc2ac519646cf5a73c6b5939906629ce6608
evguevarra <evguevarra08@gmail.com>
2022-02-19 01:35:31 +0800

    added qr validation

--- lib/scanner_page.dart
+++ lib/scanner_page.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'dart:io';
@@ -23,6 +24,12 @@ class _ScannerPageState extends State<ScannerPage> {
   UserModel loggedInUser = UserModel();
 
   final database = FirebaseDatabase.instance.ref();
+
+  final String _collection = 'collectionName';
+
+  // final StreamBuilder<QuerySnapshot> qr = FirebaseFirestore.instance
+  //     .collection('qr')
+  //     .snapshots() as StreamBuilder<QuerySnapshot<Object?>>;
 
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? result;
@@ -68,8 +75,9 @@ class _ScannerPageState extends State<ScannerPage> {
             flex: 1,
             child: Center(
               child: (result != null)
-                  ? Text(
-                      "Barcode Type: ${describeEnum(result!.format)} Data: ${result!.code} ")
+                  ? const Text(
+                    'Attendance Successfully recorded!')
+                      //"Barcode Type: ${describeEnum(result!.format)} Data: ${result!.code} ")
                   : const Text('Scan a QR code'),
             ),
           ),
@@ -78,8 +86,19 @@ class _ScannerPageState extends State<ScannerPage> {
     );
   }
 
-  void _onQRViewCreated(QRViewController controller) {
+  Future<void> _onQRViewCreated(QRViewController controller) async {
     //final attendance = database.child('attendance');
+
+    var collection = FirebaseFirestore.instance.collection('qr');
+    var docSnapshot = await collection.doc('employeeQr').get();
+    var qrVal;
+    if (docSnapshot.exists) {
+      Map<String, dynamic> data = docSnapshot.data()!;
+
+      // You can then retrieve the value from the Map like this:
+      qrVal = data['qrValue'];
+    }
+
     var now = DateTime.now();
     var formatter = DateFormat('yyyy-MM-dd');
     String formattedDate = formatter.format(now);
@@ -92,37 +111,62 @@ class _ScannerPageState extends State<ScannerPage> {
       setState(() {
         result = scanData;
         try {
-          if (widget.attendanceStatus == 'Time in') {
-            database
-                .child('attendance')
-                .child(formattedDate)
-                .child(loggedInUser.uid.toString())
-                .set({
-              "empId": loggedInUser.uid,
-              "lastName": loggedInUser.lastName,
-              "firstName": loggedInUser.firstName,
-              "timeIn": formattedTime,
-              "timeOut": '-'
-            });
-          } else if (widget.attendanceStatus == 'Time out') {
-            database
-                .child('attendance')
-                .child(formattedDate)
-                .child(loggedInUser.uid.toString())
-                .update({
-              "timeOut": formattedTime
-            });
+          if (result!.code.toString() == qrVal) {
+            if (widget.attendanceStatus == 'Time in') {
+              database
+                  .child('attendance')
+                  .child(formattedDate)
+                  .child(loggedInUser.uid.toString())
+                  .set({
+                "empId": loggedInUser.uid,
+                "lastName": loggedInUser.lastName,
+                "firstName": loggedInUser.firstName,
+                "timeIn": formattedTime,
+                "timeOut": '-'
+              });
+            } else if (widget.attendanceStatus == 'Time out') {
+              database
+                  .child('attendance')
+                  .child(formattedDate)
+                  .child(loggedInUser.uid.toString())
+                  .update({"timeOut": formattedTime});
+            }
+          } else {
+            Fluttertoast.showToast(msg: "Invalid Qr code!",toastLength: Toast.LENGTH_SHORT);
+            result = null;
           }
         } catch (e) {
           print(e);
         }
-        // print(loggedInUser.firstName);
-        // print(loggedInUser.lastName);
-        // print(loggedInUser.uid);
-        // print(widget.attendanceStatus);
-        //print(loggedInUser.lastName);
       });
     });
+  }
+
+  Future<void> _showNoQrDialog() async {
+    return showDialog<void>(
+      context: context,
+      //barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Error'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('This Qr does not exist in the database'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
