8eda0dafa690adbc749818aed18c0ac8fe4422c3
evguevarra <evguevarra08@gmail.com>
2022-03-19 03:50:40 +0800

    added late notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -6,11 +7,43 @@ import 'views/loginpage.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  AwesomeNotifications().initialize(
+    null,
+    [
+      NotificationChannel(
+        channelKey: 'key1',
+        channelName: 'Reminders',
+        channelDescription: 'You are running late for your work shift!',
+        defaultColor: Colors.red,
+        ledColor: Colors.white,
+        playSound: true,
+        enableLights: true,
+        enableVibration: true,
+        importance: NotificationImportance.High,
+      )
+    ]
+  );
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState(){
+    super.initState();
+    Notify();
+    AwesomeNotifications().createdStream.listen((notification) { 
+      // ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(
+      //   'Notification Created'
+      // ),));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,5 +55,20 @@ class MyApp extends StatelessWidget {
       ),
       home: const LoginPage(title: 'Flutter Demo Home Page'),
     );
+  }
+
+  void Notify() async{
+    String timezone = await AwesomeNotifications().getLocalTimeZoneIdentifier();
+    await AwesomeNotifications().createNotification(
+      content: NotificationContent(
+        id: 1,
+        channelKey:'key1',
+        title: 'Reminders',
+        body: 'You are running late for your work shift!',
+        notificationLayout: NotificationLayout.BigPicture,
+       ),
+       schedule: NotificationCalendar(hour: 8, minute: 0,repeats: true)
+       
+       );
   }
 }
